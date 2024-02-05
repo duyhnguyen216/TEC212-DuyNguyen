3585f1c77823c057eae1cc730a83a60f6080f941
donbreth <157042648+donbreth@users.noreply.github.com>
2024-02-05 13:05:45 -0600

    Add files via upload

---
+++ AddingRecursively2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class AddingRecursively2
+{
+   public static void main(String[] args)
+   {
+      Scanner input = new Scanner(System.in);
+      int x, y;
+      int result;
+      String userInput;
+      System.out.print("Enter a number >> ");
+      userInput = input.nextLine();
+      x = Integer.parseInt(userInput);
+      System.out.print("Enter another number that is larger >> ");
+      userInput = input.nextLine();
+      y = Integer.parseInt(userInput);
+      while(y < x)
+      {
+         System.out.print("Second number must be larger >> ");
+         userInput = input.nextLine();
+         y = Integer.parseInt(userInput);
+      }
+      result = sum(x, y);
+      System.out.println("The sum of all the numbers between ");
+      System.out.println("     " + x + " and " + y + " is " + result);
+   }
+   public static int sum(int start, int end)
+   {
+      if(end > start) 
+         return(end + sum(start, end - 1));
+      else
+         return end;
+   }
+}
